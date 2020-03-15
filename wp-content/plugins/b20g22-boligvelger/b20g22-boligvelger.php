f31bf0c2b3de9f02aa9a58f60ad8460a5993280b
le-Q <32103046+le-Q@users.noreply.github.com>
2020-03-15 13:28:42 +0100

    Added simple plugin setup

---
+++ wp-content/plugins/b20g22-boligvelger/b20g22-boligvelger.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Plugin Name: Interaktiv Bolig Velger
+Description: Dette er en interaktiv boligvelger av bachelorgruppe 22
+Version: 1.0.0
+Author: Gruppe 22
+License: GPLv2 or later
+Text Domain: b20g22-boligvelger
+*/
+
+defined('ABSPATH') or die('Hello, why are you here?');
+
+// activation
+
+// deactivation
+
+
